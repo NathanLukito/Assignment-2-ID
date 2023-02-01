33277abe8bb76d37e996b8423221ba22505b96a5
limjunhui <110986279+ljunhui05@users.noreply.github.com>
2023-02-01 22:50:48 +0800

    Add password checker function

--- login/login.js
+++ login/login.js
@@ -1,27 +1,55 @@
 $(document).ready(function(){
+
+    const listItems = document.querySelector('.menu').children
+    const listArray = Array.from(listItems)
+    $(".nav-btn").click(function(index){
+        var interval = 100      
+        listArray.forEach(function (i, index) {
+            setTimeout(function () {
+                i.classList.toggle("show")
+            }, index * interval)
+        })
+    })
+
+    $(".navbar").click(function(){
+        if (document.querySelector(".icon1").getAttribute("class").endsWith("topAnim"))
+        { 
+            document.querySelector(".menu-btn").innerHTML = "Menu"
+        }
+
+        else if (document.querySelector(".icon1").getAttribute("class").endsWith("icon1"))
+        {
+            document.querySelector(".menu-btn").innerHTML = "Close"
+            console.log($(".nav-btn"))
+        }
+
+        $(".icon1").toggleClass("topAnim")
+        $(".icon2").toggleClass("midAnim")
+        $(".icon3").toggleClass("botAnim")
+    })
+
     $(".login-btn").click(function(){
+        event.preventDefault()
         let name = $("#Username").val();
         let pass = $("#Password").val();
 
-        let userList = JSON.parse(localStorage.getItem(localStorage.key(1)))
-        
+        let userList = JSON.parse(localStorage.getItem("userlist"))
+
         for (let i = 0; i < userList.length; i++)
         {
             if (name == userList[i].Username && pass == userList[i].Password)
             {
-                alert("hello")
+                
+                document.querySelector(".login-btn").setAttribute("value", "Logging in...")  
+                setTimeout(function(){
+                    location.href = "/homepage/index.html"
+                },1000)
+               
             }
 
-            else if(name == userList[i].Username && pass != userList[i].Password){
-                alert("Wrong password was entered")
-            }
-
-            else if(name != userList[i].Username && pass == userList[i].Password){
-                alert("Wrong username was entered")
-            }
-
-            else if (name != userList[i].Username && pass == userList[i].Password){
-                alert("Wrong password and username was entered")
+            else if (name != userList[i].Username || pass != userList[i].Password)
+            {
+                continue
             }
         }
     })
