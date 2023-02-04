8692a0be67497d48b47f86e6ac63a9269acb8daa
NathanLukito <S10244400@connect.np.edu.sg>
2023-02-04 23:06:11 +0800

    Add book object data to booklist.html

--- booklist/booklist.js
+++ booklist/booklist.js
@@ -26,7 +26,33 @@ $(document).ready(function(){
         $(".icon3").toggleClass("botAnim")
     })
 
-    // $(".book-container").click(function(){
-    //     $(".booklist").toggleClass("book-show")
-    // })
+    let booklist = []
+    booklist = JSON.parse(localStorage.getItem("booklist"))
+    console.log(booklist)
+
+    for(let i = 0; i < booklist.length; i++)
+    {
+        root = document.querySelector(".booklist")
+        let book_container = document.createElement("div")
+        book_container.classList.add("book-container")
+
+        book_container.innerHTML = 
+        `
+            <div class = "book-container">
+                <div class = "book">
+                    <h1>${booklist[i].Title}</h1>
+                    <img class = "book-cover" src = "https://nathaninteractivedev-4002.restdb.io/media/${booklist[i].BookCover}">
+                    <p class = "synopsis">${booklist[i].Synopsis}</p>
+                </div>
+                <div class = "book-details">
+                    <h1>${booklist[i].Likes} Likes</h1>
+                    <h1>${booklist[i].Date.substring(0,10).replace("/", "-")}</h1>
+                    <h1>By: ${booklist[i].Author}</h1>
+                    <h1>Genre: ${booklist[i].Genre}</h1>
+                </div>
+            </div>
+        `
+        root.appendChild(book_container)
+    }
+    
 })
