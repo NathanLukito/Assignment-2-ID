634b5be96d7083520c46eee42eee330d672f7fd8
NathanLukito <S10244400@connect.np.edu.sg>
2023-02-05 22:41:00 +0800

    update mobile view for booklist.html

--- booklist/booklist.js
+++ booklist/booklist.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     document.querySelector(".searchfor").innerHTML = "Searched for: " + searchfor
     for(let i = 0; i < booklist.length; i++)
     {
-        if((booklist[i].Title.toUpperCase().replaceAll(" ", "")).includes(searchfor.toUpperCase().replaceAll(" ", "")) == true)
+        if(searchfor == null)
         {
             root = document.querySelector(".booklist")
             let book_container = document.createElement("div")
@@ -64,9 +64,37 @@ $(document).ready(function(){
                     <h1>Genre: ${booklist[i].Genre}</h1>
                 </div> 
             `
-            
             root.appendChild(book_container)
         }
+
+        else if((booklist[i].Title.toUpperCase().replaceAll(" ", "")).includes(searchfor.toUpperCase().replaceAll(" ", "")) == true)
+        {
+            root = document.querySelector(".booklist")
+            let book_container = document.createElement("div")
+            book_container.classList.add("book-container")
+            book_container.setAttribute("data-link", booklist[i].BookID)
+            book_container.addEventListener('click', function(){
+                let bookid = book_container.getAttribute("data-link")
+                localStorage.setItem("BookID", bookid)
+                location.href = '/homepage/book/book.html'
+            })                
+            book_container.innerHTML = 
+            ` 
+                <div class = "book">
+                    <h1>${booklist[i].Title}</h1>
+                    <img class = "book-cover" src = "https://nathaninteractivedev-4002.restdb.io/media/${booklist[i].BookCover}">
+                    <p class = "synopsis">${booklist[i].Synopsis}</p>
+                </div>
+                <div class = "book-details">
+                    <h1>${booklist[i].Likes} Likes</h1>
+                    <h1>${booklist[i].Date.substring(0,10).replace("/", "-")}</h1>
+                    <h1>By: ${booklist[i].Author}</h1>
+                    <h1>Genre: ${booklist[i].Genre}</h1>
+                </div> 
+            `
+            root.appendChild(book_container)
+        }
+        
         else
         {
             continue
