b05a7df959cebc3d0662275b7695dbc9cc0f56ae
NathanLukito <S10244400@connect.np.edu.sg>
2023-02-08 00:03:54 +0800

    Add publish form to publish.html

---
+++ publish/publish.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){   
+    const listItems = document.querySelector('.menu').children
+    const listArray = Array.from(listItems)
+    $(".nav-btn").click(function(index){
+        var interval = 100      
+        listArray.forEach(function (i, index) {
+            setTimeout(function () {
+                i.classList.toggle("show")
+            }, index * interval)
+        })
+    })
+
+    $(".nav-btn").click(function(){
+        if (document.querySelector(".icon1").getAttribute("class").endsWith("topAnim"))
+        { 
+            document.querySelector(".menu-btn").innerHTML = "Menu"
+        }
+
+        else if (document.querySelector(".icon1").getAttribute("class").endsWith("icon1"))
+        {
+            document.querySelector(".menu-btn").innerHTML = "Close"
+        }
+
+        $(".icon1").toggleClass("topAnim")
+        $(".icon2").toggleClass("midAnim")
+        $(".icon3").toggleClass("botAnim")
+    })
+
+    $(".search-icon").click(function(){
+        let search = $("#search").val()
+        localStorage.setItem("search", search)
+        location.href = '/book+authorlist/book_author.html'
+    })
+})
